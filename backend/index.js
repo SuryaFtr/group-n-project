7093eee7d485c6e265bef082204ff41389ff361d
Surya Faturohman <suryafaturohman@gmail.com>
2023-12-08 01:04:43 +0700

    feat: add register user & it validation

--- backend/index.js
+++ backend/index.js
@@ -11,8 +11,6 @@ const eventrouter = require('./routes/event')
 require('dotenv').config()
 const PORT = process.env.PORT || 3000
 
-applyMiddleware(app)
-
 const mongoose = require('mongoose')
 mongoose
     .connect(process.env.MONGO_URL)
@@ -20,11 +18,13 @@ mongoose
     .catch((err) => console.log(err))
 
 
-app.use("/auth", authrouter)
-app.use("/user", userrouter)
-app.use("/program", programrouter)
-app.use("/news", newsrouter)
-app.use("/event", eventrouter)
+applyMiddleware(app)
+
+app.use("/api/v1/auth", authrouter)
+app.use("/api/v1/user", userrouter)
+app.use("/api/v1/program", programrouter)
+app.use("/api/v1/news", newsrouter)
+app.use("/api/v1/event", eventrouter)
 
 const server = app.listen(PORT, () => {
     console.log('Server listening on port ' + PORT)
---
+++ backend/middleware/validator.js
@@ -0,0 +1,21 @@
+const { check, validationResult } = require('express-validator');
+
+exports.validateRegister = [
+    check('email', 'Invalid email')
+        .isEmail(),
+    check('username', 'username does not Empty').not().isEmpty(),
+    check('username', 'Name length should be 3 to 20 characters')
+        .isLength({ min: 3, max: 20 }),
+    check('password', 'Password does not Empty').not().isEmpty(),
+    check('password', 'Password length min should be 8 characters')
+        .isLength({ min: 8 }),
+    check('password', 'Password must contain at least one uppercase letter, one lowercase letter and one number')
+        .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]/),
+    (req, res, next) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty())
+            return res.status(422).json({ errors: errors.array() });
+        next();
+    },
+]
+
