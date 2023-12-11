a61d5d4fd4bd3872a1c39cb71480ac1e190aa69f
Surya Faturohman <suryafaturohman@gmail.com>
2023-12-11 22:10:44 +0700

    feat: #18 Add feature user profile/detail

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -4,13 +4,15 @@ const { Router } = require("express");
 const router = Router();
 
 
-//get user by id (admin, staff & member) 
-router.get("/:id", permission.is_authenticated, userController.getUserById);
+//get user profile (admin, staff & member) 
+router.get("/profile", permission.is_authenticated, userController.getUserProfile);
 //update user data (admin, staff & member) 
 router.put("/:id", permission.is_authenticated, userController.updateUserData);
 //update user password (admin, staff & member) 
 router.put("/update-password/:id", permission.is_authenticated, userController.updateUserPassword);
 
+//get user by id (admin) 
+router.get("/:id", permission.is_admin, userController.getUserById);
 //update user role (admin)
 router.put("/update-role/:id", permission.is_admin, userController.updateUserRole);
 //get all user (admin)
