71821f651d9ece6e44ec354af0776155d514a0d9
Surya Faturohman <suryafaturohman@gmail.com>
2023-12-14 17:29:54 +0700

    feat: #37 Add feature create program

---
+++ backend/controllers/programController.js
@@ -0,0 +1,25 @@
+const { Program } = require("../models/Program");
+
+const dateNow = new Date();
+
+exports.createProgram = async (req, res) => {
+    const {
+        pictureLink,
+        title,
+        description,
+        programDate
+    } = req.body;
+
+    const checkTitle = await Program.findOne({ title: title });
+    if (checkTitle) {
+        res.status(400).json({ error: "Program title is already exists" });
+        return;
+    }
+
+    const program = await Program.create({ pictureLink, title, description, programDate });
+
+    program.publishedBy = req.user._id;
+
+    await program.save();
+    res.status(201).json({ message: "Program successfully created" });
+}
