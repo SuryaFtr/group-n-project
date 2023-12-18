229399ab4c05c9645364de9dcd005b16d83117c5
Surya Faturohman <suryafaturohman@gmail.com>
2023-12-18 15:05:34 +0700

    feat: #65 Add feature update event

--- backend/routes/event.js
+++ backend/routes/event.js
@@ -10,9 +10,9 @@ router.post("", permission.is_adminOrStaff, validateEvent, eventController.creat
 //get all events
 router.get("", eventController.getAllEvent);
 //get event by id (admin, staff & member) 
-router.get("/:id", eventController.getEventById);
+router.get("/:id", permission.is_authenticated, eventController.getEventById);
 //update event (admin & staff)
-router.put("/:id");
+router.put("/:id", permission.is_adminOrStaff, validateEvent, eventController.updateEvent);
 //delete event (admin & staff)
 router.delete("/:id");
 
